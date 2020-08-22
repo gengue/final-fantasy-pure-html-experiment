cdc99db7a9249d9050851d6d37ae3933e1d3e406
Genesis Guerrero <genesisdaft@gmail.com>
2020-08-21 19:14:08 -0500

    Updates

---
+++ precache-manifest.f161b45f56e8f7bb0dfdda5ee2536452.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdfa6560c387c408b7b7f00ea9a1c8f",
+    "url": "/final-fantasy-pure-html-experiment/index.html"
+  },
+  {
+    "revision": "ad9efbfd54c1dcdf16fb",
+    "url": "/final-fantasy-pure-html-experiment/static/css/main.6baded23.chunk.css"
+  },
+  {
+    "revision": "8934ce8bcdc71809bdad",
+    "url": "/final-fantasy-pure-html-experiment/static/js/2.79b9df45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/final-fantasy-pure-html-experiment/static/js/2.79b9df45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9efbfd54c1dcdf16fb",
+    "url": "/final-fantasy-pure-html-experiment/static/js/main.50f28cea.chunk.js"
+  },
+  {
+    "revision": "4900488b168080e8328c",
+    "url": "/final-fantasy-pure-html-experiment/static/js/runtime-main.a01ad3ff.js"
+  },
+  {
+    "revision": "238f74a1f84f2f241b085f6ac09df4c4",
+    "url": "/final-fantasy-pure-html-experiment/static/media/cloud.238f74a1.gif"
+  },
+  {
+    "revision": "b50ac62fee1f730136a9264f2f7a2bc4",
+    "url": "/final-fantasy-pure-html-experiment/static/media/dragon.b50ac62f.gif"
+  }
+]);
